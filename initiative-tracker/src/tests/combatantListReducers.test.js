301d4fc4d9c67a0239dc1c42101c0f6487c3acae
pjmaddox24 <pjmaddox24@gmail.com>
2018-08-06 15:45:23 -0500

    beginning reducer work

---
+++ initiative-tracker/src/tests/combatantListReducers.test.js
@@ -0,0 +1,7 @@
+import { combatantReducer } from "../stores/reducers/combatantListReducers.js";
+
+describe("initiativeApp", () => {
+    it("should return an empty llist of combatants when no current state given", () => {
+        
+    });
+});
