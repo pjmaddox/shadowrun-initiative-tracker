ee160b36f4599b3d797dade58288c307e3e67716
pjmaddox24 <pjmaddox24@gmail.com>
2018-08-12 15:05:41 -0500

    -nailed it

--- initiative-tracker/bash-scripts/test.sh
+++ initiative-tracker/bash-scripts/test.sh
@@ -5,7 +5,7 @@ echo "Generating latest build!"
 yarn build
 
 #Remove the current stuff on the server and stop the current docker container
-SCRIPT="echo '---Removing dockertest dir---\n';sudo rm -r dockertest; echo ---Removing active docker instances---\n; docker ps -aq; echo ---Remaking directory to hold app---\n; mkdir dockertest; cd dockertest; mkdir build"
+SCRIPT="echo '---Removing dockertest dir---\n';sudo rm -r dockertest; echo ---Stopping active docker instances---\n; docker kill init-app;  echo ---Removing all docker instances---\n; docker rm init-app; echo ---Remaking directory to hold app---\n; mkdir dockertest; cd dockertest; mkdir build"
 ssh -i workLaptopEc2KeyPair.pem ec2-user@18.222.222.150 "${SCRIPT}"
 
 #remove all the node modules
@@ -13,12 +13,13 @@ ssh -i workLaptopEc2KeyPair.pem ec2-user@18.222.222.150 "${SCRIPT}"
 
 #Transfer the needed files to the server
 echo "---Sending files for docker to build---\n"
-scp -i workLaptopEc2KeyPair.pem -r ./build/ ec2-user@18.222.222.150:~/dockertest/build
+scp -i workLaptopEc2KeyPair.pem -r ./build/ ec2-user@18.222.222.150:~/dockertest
 scp -i workLaptopEc2KeyPair.pem -r ./.dockerignore ec2-user@18.222.222.150:~/dockertest
 scp -i workLaptopEc2KeyPair.pem -r ./Dockerfile ec2-user@18.222.222.150:~/dockertest
 scp -i workLaptopEc2KeyPair.pem -r ./server.js ec2-user@18.222.222.150:~/dockertest
+scp -i workLaptopEc2KeyPair.pem -r ./package.json ec2-user@18.222.222.150:~/dockertest
 
-SCRIPT2="echo '---Building docker container---\n; cd dockertest; docker build -t pjmaddox24/initiative-tracker-image .; echo '---Running docker container -p 80:8080---\n'; docker run -d -p 80:8080 --name init-app pjmaddox24/initiative-tracker-image"
+SCRIPT2="echo '---Building docker container---\n'; cd dockertest; docker build -t pjmaddox24/initiative-tracker-image .; echo '---Running docker container -p 80:8080---\n'; docker run -d -p 80:8080 --name init-app pjmaddox24/initiative-tracker-image"
 ssh -i workLaptopEc2KeyPair.pem ec2-user@18.222.222.150 "${SCRIPT2}"
 
 echo "---Complete!---\n"
--- initiative-tracker/src/App.js
+++ initiative-tracker/src/App.js
@@ -9,13 +9,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React2</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <div>
-
+        <div className="combatantAppContainer">
+            <h2>This is the container for the app itself.</h2>
         </div>
       </div>
     );
